e8d43fc792cd70772d7fa48764804ba548aa715a
fhumur <rakhunwanafhumulani@gmail.com>
2023-07-12 07:30:18 +0000

    deleted

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def is_kind_of_class(obj, a_class):
-    """
-    return True if object is instance of class or inherited from
-    return Falase otherwise
-    """
-    if isinstance(obj, a_class):
-        return True
-    return False
